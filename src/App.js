15a2fa7b597dcbedfc030f61f4739111b01ca005
Colin Smith <jayson.colin.smith@gmail.com>
2022-07-07 17:11:44 -0400

    Removed api import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CitySelector from './components/CitySelector/CitySelector.js';
 import './App.css';
 import {Container} from 'react-bootstrap';
 import UseFetch from './hooks/UseFetch';
-import {API_KEY, API_BASE_URL} from './apis/config'
+//import {API_KEY, API_BASE_URL} from './apis/config'
 import WeatherList from './components/WeatherList.js';
 
 const App = () => {
